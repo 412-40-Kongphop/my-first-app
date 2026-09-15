9b2384c7343229efe4288c6875ef6ffe8405de59
412-40-Kongphop <56878@yupparaj.ac.th>
2026-09-15 10:53:44 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -56,13 +56,14 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
 
 
     # ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มตรวจข้อ 3, 4 ตรงนี้
-     if u_ans3 == "glasses":
+    
+    if u_ans3 == "glasses":
         st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
 
-     if u_ans4 == "coconut":
+    if u_ans4 == "coconut":
         st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
     else:
